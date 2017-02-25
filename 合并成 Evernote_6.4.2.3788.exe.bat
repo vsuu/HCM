638506ec68d4c0115b6f0b26ec092a0d87064615
vsuu <rftdmail@gmail.com>
2017-02-25 15:29:04 +0800

    Delete 合并成 Evernote_6.4.2.3788.exe.bat

--- 合并成 Evernote_6.4.2.3788.exe.bat
+++
@@ -1,1 +0,0 @@
-copy/b "Evernote_6.4.2.3788.exe.001"+"Evernote_6.4.2.3788.exe.002"+"Evernote_6.4.2.3788.exe.003"+"Evernote_6.4.2.3788.exe.004"+"Evernote_6.4.2.3788.exe.005" "Evernote_6.4.2.3788.exe"
